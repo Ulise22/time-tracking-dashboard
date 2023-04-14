97c87ece232ebf0b7de875b4218c37ce2a232f47
Ulises Montenegro <ulimontenegro23@gmail.com>
2023-04-14 16:42:05 -0300

    image position fixed

--- app/Card.js
+++ app/Card.js
@@ -4,11 +4,11 @@ import Image from 'next/image'
 export default function Card ({item, index, view}) {
 
     return(
-        <article className='relative'>
-            <section style={{background: objectStyle[index].color}} className={`flex z-0 justify-end rounded-t-xl `}>
-                <Image className='z-0' src={objectStyle[index].img} alt='#' height={80} />
+        <div className='relative'>
+            <section style={{background: objectStyle[index].color}} className={`flex justify-end rounded-t-xl `}>
+                <Image src={objectStyle[index].img} alt='#' height={80} />
             </section>
-            <section className='grid z-20 gap-4 bg-darkBlue hover:bg-desaturatedBlue -mt-6 p-7 rounded-xl h-46 cursor-pointer lg:absolute w-full lg:w-64'>
+            <section className='grid z-20 gap-4 bg-darkBlue hover:bg-desaturatedBlue -mt-6 p-7 rounded-xl h-46 cursor-pointer relative lg:absolute w-full lg:w-64'>
                 <div className='flex items-center justify-between'>
                     <h4 className='font-medium text-lg'> {item.title} </h4>
                     <p className='text-paleBlue text-lg font-bold hover:text-white cursor-pointer'>...</p>
@@ -27,6 +27,6 @@ export default function Card ({item, index, view}) {
                 }hrs
                     </p>
             </section>
-        </article>
+        </div>
     )
 }
